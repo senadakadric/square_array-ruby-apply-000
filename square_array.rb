0335b9d86b5d9b7753821143ed574054ff597206
Senada Kadric <skadric@fordham.edu>
2020-02-14 19:06:14 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,21 @@
 def square_array(array)
-  # your code here
+
+  squarred = []
+
+# using a while loop to square the array
+#  i = 0
+#  while i < array.length
+#    squarred << array[i] ** 2
+#
+#    i+=1
+#  end
+
+array.each do |num|
+squarred << num ** 2
 end
+
+return squarred
+
+end
+
+# square_array[1,2,3] # => [1,4,9])
