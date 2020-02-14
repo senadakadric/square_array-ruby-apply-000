9ef0f1c068b285ed08c70629c4d6854552279f57
Senada Kadric <skadric@fordham.edu>
2020-02-14 19:00:37 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,18 @@
 def square_array(array)
-  # your code here
+
+  squarred = []
+  numsquare = 1
+
+  i = 0
+  while i < array.length
+    numsquare = array[i] **2
+    squarred << numsquare
+
+    i+=1
+  end
+
+  return squarred
+
 end
+
+puts square_array[1,2,3] # => [1,4,9])
